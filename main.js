d9197841808fc020b25840934957598a88f7c0e0
yudai <yudai.0702wa@gmail.com>
2019-08-29 14:44:13 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+
+  // ブラウザ(Chromium)の起動, 初期画面のロード
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
