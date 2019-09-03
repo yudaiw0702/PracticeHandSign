76dcb684b20ed7a0bfd2454f4953124fdc30cf0d
yudai <yudai.0702wa@gmail.com>
2019-09-03 20:20:30 +0900

    cubeを削除

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/title.html');
+  mainWindow.loadURL('file://' + __dirname + '/html/title.html');
 
   mainWindow.on('closed', function() {
     mainWindow = null;
