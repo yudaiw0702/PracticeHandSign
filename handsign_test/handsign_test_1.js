14f077c6dd1c944c7a6d330d6288f97db7fb3fb8
Yudai Watanabe <b1601824@planet.kanazawa-it.ac.jp>
2019-10-01 16:06:37 +0900

    吹き出しアニメーションの追加

---
+++ handsign_test/handsign_test_1.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  let $balloon_left = $(".balloon1-left");
+  let $balloon_right = $(".balloon1-right");
+
+  $balloon_left.toggleClass("balloon_up");
+  $balloon_right.toggleClass("balloon_up");
+
+};
