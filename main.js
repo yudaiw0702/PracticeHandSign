cd7d35c9bedd88e9cba1ad2339bbcb22e37eeb70
yudai <yudai.0702wa@gmail.com>
2019-10-30 18:19:07 +0900

    テストの文字変更＋精度向上

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 900, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/html/title.html');
 
   mainWindow.on('closed', function() {
